8791cb967e7a0b65ec124940e012ef1ad44023f9
saintmrti <saint-martin_10@hotmail.com>
2023-08-08 02:10:47 -0600

    agregamos nuevos proyectos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,100 +1,165 @@
 import React from "react";
-import Logo from "../assets/logo.png"
-import {FaBars, FaTimes, FaGithub, FaLinkedin, FaMailBulk, FaWhatsapp} from "react-icons/fa";
-import {BsFillPersonLinesFill} from "react-icons/bs";
-import {useState} from "react";
-import {Link} from "react-scroll";
+import Logo from "../assets/logo.png";
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaMailBulk,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { useState } from "react";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-    const [nav, setNav] = useState(false)
-
-    const handleClick = () => setNav(!nav)
-
-   
+  const handleClick = () => setNav(!nav);
 
   return (
-      <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-          <div>
-              <img src={Logo} alt="logo" style={{width: '70px'}}/>
-          </div>
-
-          {/* menu */}
-          <ul className="hidden md:flex">
-            <li className="hover:text-pink-600"><Link to="home" smooth={true} duration={500}>Home</Link></li>
-            <li className="hover:text-pink-600"><Link to="about" smooth={true} duration={500}>About</Link></li>
-            <li className="hover:text-pink-600"><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
-            <li className="hover:text-pink-600"><Link to="work" smooth={true} duration={500}>Work</Link></li>
-            <li className="hover:text-pink-600"><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
-          </ul>
-
-          {/* hamburger */}
-          <div onClick={handleClick} className="md:hidden z-10">
-              {!nav ? <FaBars /> : <FaTimes />}
-          </div>
-
-          {/* mobile menu */}
-          <ul className={!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"}>
-              <li className='py-6 text-4xl hover:text-pink-600'><Link onClick={handleClick} to='home' smooth={true} duration={1000}>Home</Link></li>
-              <li className='py-6 text-4xl hover:text-pink-600'>
-                  {' '}
-                  <Link onClick={handleClick} to='about' smooth={true} duration={1000}>
-                      About
-                  </Link>
-              </li>
-              <li className='py-6 text-4xl hover:text-pink-600'>
-                  {' '}
-                  <Link onClick={handleClick} to='skills' smooth={true} duration={1000}>
-                      Skills
-                  </Link>
-              </li>
-              <li className='py-6 text-4xl hover:text-pink-600'>
-                  {' '}
-                  <Link onClick={handleClick} to='work' smooth={true} duration={1000}>
-                      Work
-                  </Link>
-              </li>
-              <li className='py-6 text-4xl hover:text-pink-600'>
-                  {' '}
-                  <Link onClick={handleClick} to='contact' smooth={true} duration={1000}>
-                      Contact
-                  </Link>
-              </li>
-          </ul>
-          {/* social icons */}
-          <div className="lg:flex flex fixed flex-col top-[35%] left-0">
-              <ul>
-                  <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-blue-800">
-                      <a className="flex justify-between items-center w-full text-gray-300" href="https://www.linkedin.com/in/saintmartin/" target="_blank" rel="noreferrer">
-                          LinkedIn <FaLinkedin className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]"/>
-                      </a>
-                  </li>
-                  <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-blue-500">
-                      <a className="flex justify-between items-center w-full text-gray-300" href="mailto:saint-martin_10@hotmail.com?body=Hola,%20Saint">
-                          Mail <FaMailBulk className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]"/>
-                      </a>
-                  </li>
-                  <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-black">
-                      <a className="flex justify-between items-center w-full text-gray-300" href="https://github.com/saintmrti" target="_blank" rel="noreferrer">
-                          GitHub <FaGithub className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]"/>
-                      </a>
-                  </li>
-                  <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-green-500">
-                      <a className="flex justify-between items-center w-full text-gray-300" href="https://wa.me/2288548416" target="_blank" rel="noreferrer">
-                          WhatsApp <FaWhatsapp className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]"/>
-                      </a>
-                  </li>
-                  <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-                      <a className="flex justify-between items-center w-full text-gray-300" href="https://drive.google.com/file/d/1m3yEkoSnC9Ieh5xltfMA4g9MGLtCmokf/view?usp=sharing" target="_blank" rel="noreferrer">
-                          Curriculum <BsFillPersonLinesFill className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]"/>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-
-
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      <div>
+        <img src={Logo} alt="logo" style={{ width: "70px" }} />
       </div>
-  );
-}
 
-export default Navbar
+      {/* menu */}
+      <ul className="hidden md:flex">
+        <li className="hover:text-pink-600">
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="hover:text-pink-600">
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="hover:text-pink-600">
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="hover:text-pink-600">
+          <Link to="work" smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="hover:text-pink-600">
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl hover:text-pink-600">
+          <Link onClick={handleClick} to="home" smooth={true} duration={1000}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl hover:text-pink-600">
+          {" "}
+          <Link onClick={handleClick} to="about" smooth={true} duration={1000}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl hover:text-pink-600">
+          {" "}
+          <Link onClick={handleClick} to="skills" smooth={true} duration={1000}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl hover:text-pink-600">
+          {" "}
+          <Link onClick={handleClick} to="work" smooth={true} duration={1000}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl hover:text-pink-600">
+          {" "}
+          <Link
+            onClick={handleClick}
+            to="contact"
+            smooth={true}
+            duration={1000}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+      {/* social icons */}
+      <div className="lg:flex flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-blue-800">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/saintmartin/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn{" "}
+              <FaLinkedin className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]" />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-blue-500">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:saint-martin_10@hotmail.com?body=Hola,%20Saint"
+            >
+              Mail{" "}
+              <FaMailBulk className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]" />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-black">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/saintmrti"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub{" "}
+              <FaGithub className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]" />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-green-500">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://wa.me/2288548416"
+              target="_blank"
+              rel="noreferrer"
+            >
+              WhatsApp{" "}
+              <FaWhatsapp className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]" />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center lg:ml-[-100px] ml-[-120px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://drive.google.com/file/d/1Xq5VUTKSKW20fIrK1HCVCvR8x_jp3_cw/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Curriculum{" "}
+              <BsFillPersonLinesFill className="lg:w-[30px] lg:h-[30px] w-[15px] h-[15px]" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/components/Work.js
+++ src/components/Work.js
@@ -18,37 +18,6 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${GymBros})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://proyectogymbase.web.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/saintmrti/ProyectoGymBros.git"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
 
           <div
             style={{ backgroundImage: `url(${Viakable})` }}
@@ -99,6 +68,38 @@ const Work = () => {
                 </a>
                 <a
                   href="https://github.com/saintmrti/ProyectoPlantReadiness.git"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${GymBros})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://proyectogymbase.web.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/saintmrti/ProyectoGymBros.git"
                   target="_blank"
                   rel="noreferrer"
                 >
